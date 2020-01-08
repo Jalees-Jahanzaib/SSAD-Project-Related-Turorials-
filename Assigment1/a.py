cc684e28e0d6edac0e56c8feeb22006250eb9811
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 20:14:21 +0530

    Terminal Game 6

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -1,18 +1,41 @@
 # Curses uses cursor in terminal;
 import curses
 import time
+menu=['Home','Play','Scoreboard','Exit']
+def print_menu(stdscr,selected_row_idx):
+    stdscr.clear()
+    h,w=stdscr.getmaxyx()
+    for idx,row in enumerate(menu):
+        x=w//2-len(row)//2
+        y=h//2-len(menu)//2+idx
+        if idx == selected_row_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y,x,row)
+            stdscr.attroff(curses.color_pair(1))
+
+        else:
+            stdscr.addstr(y,x,row)
+
+    stdscr.refresh()
 
 def main(stdscr):
     curses.curs_set(0)
+    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_WHITE)
+    current_row=0
+    print_menu(stdscr,current_row)
+
     while 1:
         key=stdscr.getch()
         stdscr.clear()
-        if key==curses.KEY_UP:
-            stdscr.addstr(0,0,"You Pressed UP Key!!")
-        elif key==curses.KEY_DOWN:
-            stdscr.addstr(0,0,"You Pressed DOWN Key!!")
+        if key==curses.KEY_UP and current_row > 0:
+            current_row-=1;
+        elif key==curses.KEY_DOWN and current_row <len(menu)-1:
+            current_row+=1;
         elif key==curses.KEY_ENTER or key in [10,13]:
-            stdscr.addstr(0,0,"You Pressed ENTER Key!!")    
+            stdscr.addstr(0,0,"You Pressed {}".format(menu[current_row]))
+            stdscr.refresh()
+            stdscr.getch()
+        print_menu(stdscr,current_row)    
         stdscr.refresh()
-
+       
 curses.wrapper(main) # runs main function in try and error block and returns the terminal to initial state after code is completd

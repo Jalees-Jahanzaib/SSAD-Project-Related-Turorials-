08f169a9d684cd32d93cc766d327d5eff776d4f8
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 18:46:56 +0530

    Terminal Game 2

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -2,15 +2,9 @@
 import curses
 import time
 
-stdscr= curses.initscr() #Initizes terminal session
-curses.noecho() #no print on terminal
-curses.cbreak() # write something and it is collected and enter is not required
-stdscr.keypad(True) #activates the Keyboard for input
-
-stdscr.addstr(15,5,"Hello")
-stdscr.refresh()
-time.sleep(3)
-curses.echo() 
-curses.nocbreak() # Reseting
-stdscr.keypad(False)
-curses.endwin() # Ends terminal Session
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.addstr(15,5,"Hello")
+    stdscr.refresh()
+    time.sleep(3)
+curses.wrapper(main) # runs main function in try and error block and returns the terminal to initial state after code is completd

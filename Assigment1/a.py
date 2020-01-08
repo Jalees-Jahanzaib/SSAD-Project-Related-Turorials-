77ebbe70fbe1c81997959e281207e75599ec9440
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 21:15:02 +0530

    Terminal Game Box

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -1,44 +1,13 @@
 # Curses uses cursor in terminal;
 import curses
+from curses import textpad
 import time
-menu=['Home','Play','Scoreboard','Exit']
-def print_menu(stdscr,selected_row_idx):
-    stdscr.clear()
-    h,w=stdscr.getmaxyx()
-    for idx,row in enumerate(menu):
-        x=w//2-len(row)//2
-        y=h//2-len(menu)//2+idx
-        if idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y,x,row)
-            stdscr.attroff(curses.color_pair(1))
-
-        else:
-            stdscr.addstr(y,x,row)
-
-    stdscr.refresh()
-
 def main(stdscr):
     curses.curs_set(0)
-    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_WHITE)
-    current_row=0
-    print_menu(stdscr,current_row)
+    sh,sw=stdscr.getmaxyx()
+    box=[[3,3],[sh-3,sw-3]]
+    textpad.rectangle(stdscr,box[0][0],box[0][1],box[1][0],box[1][1])
+    stdscr.refresh()
+    stdscr.getch()
 
-    while 1:
-        key=stdscr.getch()
-        stdscr.clear()
-        if key==curses.KEY_UP and current_row > 0:
-            current_row-=1;
-        elif key==curses.KEY_DOWN and current_row <len(menu)-1:
-            current_row+=1;
-        elif key==curses.KEY_ENTER or key in [10,13]:
-            stdscr.addstr(0,0,"You Pressed {}".format(menu[current_row]))
-            stdscr.refresh()
-            stdscr.getch()
-            if current_row==len(menu)-1:
-                break;
-
-        print_menu(stdscr,current_row)    
-        stdscr.refresh()
-       
 curses.wrapper(main) # runs main function in try and error block and returns the terminal to initial state after code is completd

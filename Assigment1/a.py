ddda12dc678636e4516ce8522381edc66c434e0f
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 18:55:21 +0530

    Terminal Game 3

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -4,7 +4,12 @@ import time
 
 def main(stdscr):
     curses.curs_set(0)
-    stdscr.addstr(15,5,"Hello")
+    h,w=stdscr.getmaxyx()
+    text= "Main Menu"
+    x=int(w/2-len(text)/2)
+    y=int(h/2)
+    stdscr.addstr(y,x,text)
     stdscr.refresh()
     time.sleep(3)
+
 curses.wrapper(main) # runs main function in try and error block and returns the terminal to initial state after code is completd

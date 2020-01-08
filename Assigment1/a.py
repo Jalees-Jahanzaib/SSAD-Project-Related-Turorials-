178013a0810ac8f29e452e4ee772e03bf7a04c6f
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 18:59:50 +0530

    Terminal Game 4

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -4,11 +4,16 @@ import time
 
 def main(stdscr):
     curses.curs_set(0)
+    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_YELLOW)
     h,w=stdscr.getmaxyx()
     text= "Main Menu"
     x=int(w/2-len(text)/2)
     y=int(h/2)
+    stdscr.attron(curses.color_pair(1))
+
     stdscr.addstr(y,x,text)
+    stdscr.attroff(curses.color_pair(1))
+
     stdscr.refresh()
     time.sleep(3)
 

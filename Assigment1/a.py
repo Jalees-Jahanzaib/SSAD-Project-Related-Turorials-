773cacb370105241f33fc0d3d6666647d3745918
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 19:06:48 +0530

    Terminal Game 5

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -4,17 +4,15 @@ import time
 
 def main(stdscr):
     curses.curs_set(0)
-    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_YELLOW)
-    h,w=stdscr.getmaxyx()
-    text= "Main Menu"
-    x=int(w/2-len(text)/2)
-    y=int(h/2)
-    stdscr.attron(curses.color_pair(1))
-
-    stdscr.addstr(y,x,text)
-    stdscr.attroff(curses.color_pair(1))
-
-    stdscr.refresh()
-    time.sleep(3)
+    while 1:
+        key=stdscr.getch()
+        stdscr.clear()
+        if key==curses.KEY_UP:
+            stdscr.addstr(0,0,"You Pressed UP Key!!")
+        elif key==curses.KEY_DOWN:
+            stdscr.addstr(0,0,"You Pressed DOWN Key!!")
+        elif key==curses.KEY_ENTER or key in [10,13]:
+            stdscr.addstr(0,0,"You Pressed ENTER Key!!")    
+        stdscr.refresh()
 
 curses.wrapper(main) # runs main function in try and error block and returns the terminal to initial state after code is completd

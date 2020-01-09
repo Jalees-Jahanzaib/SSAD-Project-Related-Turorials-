7a00b5466178264c0a48f617c3f892fdf021a6a9
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-09 11:06:30 +0530

    Lect 4

--- Assigment1/a.py
+++ Assigment1/a.py
@@ -7,7 +7,6 @@ def main(stdscr):
     sh,sw=stdscr.getmaxyx()
     box=[[3,3],[sh-3,sw-3]]
     textpad.rectangle(stdscr,box[0][0],box[0][1],box[1][0],box[1][1])
-    stdscr.refresh()
-    stdscr.getch()
+    snake=[[],[],[] ]
 
 curses.wrapper(main) # runs main function in try and error block and returns the terminal to initial state after code is completd
